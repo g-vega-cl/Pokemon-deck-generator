ba41cf7d1f7f77fe45e3942dbc32b2d746218013
Cesar Vega <g.vega.cl@gmail.com>
2022-12-08 11:24:56 -0500

    Use cloud DB to avoid DB setup pains

--- Backend/main.go
+++ Backend/main.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	api := api.Api{}
-	api.Initialize("root:adminroot@tcp(127.0.0.1:3306)/Pokemon-Smalldoor")
+	// api.Initialize("root:adminroot@tcp(127.0.0.1:3306)/Pokemon-Smalldoor") // <- Local DB
+	api.Initialize("sql9583215:UYxXbd1FBI@tcp(sql9.freesqldatabase.com:3306)/sql9583215") // <- Cloud DB - Please don't abuse.
 
 	api.Router.HandleFunc("/generate", api.GenerateDeck).Methods("GET") // TODO // Check the best method for this
 	api.Router.HandleFunc("/deck", api.GetDeck).Methods("GET")
