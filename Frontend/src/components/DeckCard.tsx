b31911ce50883db52a150c2dd2ce25d08f2b8cd1
Cesar Vega <g.vega.cl@gmail.com>
2022-12-09 10:17:35 -0500

    Add name, type subtitles to deck cards

--- Frontend/src/components/DeckCard.tsx
+++ Frontend/src/components/DeckCard.tsx
@@ -8,51 +8,66 @@ import { useNavigate } from "react-router-dom";
 import { IDeckCard } from "../models/pokemon";
 
 export interface IComponentDeckCard {
-    deckCard: IDeckCard;
-    setNavbarTabValue: React.Dispatch<number>;
+  deckCard: IDeckCard;
+  setNavbarTabValue: React.Dispatch<number>;
 }
 
 export interface IComponentDeckCardCard {
-    deckCard: IDeckCard;
-    handleClick: () => void;
+  deckCard: IDeckCard;
+  handleClick: () => void;
 }
 
-export const DeckCardCard = ({deckCard, handleClick}: IComponentDeckCardCard) => {
-    return (
-        <Card sx={{ height: "100%" }} data-testid="DeckCard-CardTag">
-        <CardActionArea
-            onClick={handleClick}
-        >
-            <CardMedia
-            component="img"
-            height="100%" // Make responsive
-            width="100%"
-            image={deckCard.Image}
-            alt={deckCard.Name}
-            />
-            <CardContent sx={{ height: 150 }}>
-            <Typography gutterBottom variant="subtitle1" component="div">
-                {deckCard.Name || "Unnamed"}
-            </Typography>
-            <Typography gutterBottom variant="subtitle1" component="div">
-                {deckCard.Type}
-            </Typography>
-            </CardContent>
-        </CardActionArea>
-        </Card>
-    );
-}
+export const DeckCardCard = ({
+  deckCard,
+  handleClick,
+}: IComponentDeckCardCard) => {
+  return (
+    <Card sx={{ height: "100%" }} data-testid="DeckCard-CardTag">
+      <CardActionArea onClick={handleClick}>
+        <CardMedia
+          component="img"
+          height="100%" // Make responsive
+          width="100%"
+          image={deckCard.Image}
+          alt={deckCard.Name}
+        />
+        <CardContent sx={{ height: 150 }}>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            component="div"
+            sx={{fontWeight: "bold"}}
+          >
+            Name:
+          </Typography>
+          <Typography gutterBottom variant="subtitle2" component="div">
+            {deckCard.Name || "Unnamed"}
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            component="div"
+            sx={{fontWeight: "bold"}}
+          >
+            Type:
+          </Typography>
+          <Typography gutterBottom variant="subtitle2" component="div">
+            {deckCard.Type}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
 
 const DeckCard = ({ deckCard, setNavbarTabValue }: IComponentDeckCard) => {
-    const navigate = useNavigate();
-    const handleClick = () => {
-        setNavbarTabValue(2);
-        navigate(`/decks/${deckCard.Id}`);
-    }
-  
-    return (
-        <DeckCardCard deckCard={deckCard} handleClick={handleClick}/>
-    );
+  const navigate = useNavigate();
+  const handleClick = () => {
+    setNavbarTabValue(2);
+    navigate(`/decks/${deckCard.Id}`);
+  };
+
+  return <DeckCardCard deckCard={deckCard} handleClick={handleClick} />;
 };
 
 export default DeckCard;
